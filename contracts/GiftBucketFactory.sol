f2971e659c8ed9f60549309fc7ff8273e36a50f9
Andrea Franz <andrea@gravityblast.com>
2020-04-03 14:19:40 +0200

    re-organize contracts

--- contracts/GiftBucketFactory.sol
+++ contracts/GiftBucketFactory.sol
@@ -6,7 +6,7 @@ import "./Proxy.sol";
 contract GiftBucketFactory {
   GiftBucket public GiftBucketImplementation;
 
-  event Created(address indexed gifter, address indexed bucket);
+  event BucketCreated(address indexed gifter, address indexed bucket);
 
   constructor() public {
     GiftBucketImplementation = new GiftBucket(address(0), block.timestamp + 1);
@@ -14,7 +14,7 @@ contract GiftBucketFactory {
 
   function create(address _tokenAddress, uint256 _expirationTime) public returns (address) {
     address p = address(new Proxy(abi.encodeWithSelector(0xc350a1b5, _tokenAddress, _expirationTime, msg.sender), address(GiftBucketImplementation)));
-    emit Created(msg.sender, p);
+    emit BucketCreated(msg.sender, p);
     return p;
   }
 }
