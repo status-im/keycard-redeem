d2ea7607ebeaeb8854551ad44c732c3a08c11c2d
Andrea Franz <andrea@gravityblast.com>
2020-09-30 12:17:32 +0200

    tmp remove name from detailed erc20

--- contracts/erc20/IERC20Detailed.sol
+++ contracts/erc20/IERC20Detailed.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.5.16;
 import "./IERC20.sol";
 
 contract IERC20Detailed is IERC20 {
-  function name() public view returns (string memory);
   function symbol() public view returns (string memory);
   function decimals() public view returns (uint8);
 }
